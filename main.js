10be6013a89344e61d9e0a3de35dddcd5be58424
vitalii <dumavb777@gmail.com>
2019-05-23 16:18:45 +0300

    develop2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let data = {
+const data = {
     "Рыбы": {
         "Форель": {},
         "Щука": {}
@@ -16,17 +16,16 @@ let data = {
     }
 };
 
-
-let container = document.getElementById('container');
+const container = document.getElementById('container');
 
 function createTree(domElement, obj) {
     const ul = document.createElement('ul');
     for (const key in obj) {
         const li = document.createElement('li');
-        li.innerHTML = key;
+        li.textContent = key;
         ul.appendChild(li);
         domElement.appendChild(ul);
-        if (obj[key] !== undefined) {
+        if (obj.hasOwnProperty(key)) {
             createTree(li, obj[key])
         }
     }
